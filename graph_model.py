e62994006683b3c183530cc877e00e45e1abdb19
jasman <jasman0603@gmail.com>
2026-02-08 22:37:55 +0700

    RAG Similiarity Search and Re-rank

--- graph_model.py
+++ graph_model.py
@@ -3,6 +3,7 @@ from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_core.prompts import PromptTemplate
 from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.documents import Document
 #Chroma Vector Store
 from build_vectore import get_vector_store
 from retriever import create_retriever
@@ -15,7 +16,7 @@ from llm import create_chat
 
 
 class State(MessagesState):
-    documents: List[str]
+    documents: List[Document]
 
 
 def retrieve_documents(state: State) -> State:
@@ -28,12 +29,46 @@ def retrieve_documents(state: State) -> State:
 
     #Pinecone Vector Store
     vector_store = create_pinecone_vector_store("langchain-test-index", embeddings)
-    retriever = create_retriever_pinecone(vector_store, k=3, score_threshold=0.4)
+    retriever = create_retriever_pinecone(vector_store, k=10, score_threshold=0.4)
 
     query = state["messages"][-1].content
     docs = retriever.invoke(query)
 
-    document_pages = [doc.page_content for doc in docs]
+    # document_pages = [doc.page_content for doc in docs]
+
+    return {"documents": docs}
+
+def re_rank_documents(state: State) -> State:
+    chat = create_chat()
+    documents = state["documents"]
+    query = state["messages"][-1].content
+
+    prompt = f"""
+    You are a ranking assistant.
+
+    Given the query:
+    "{query}"
+
+    Rank the following documents by relevance.
+    Return ONLY a Python list of integer indices (e.g. [2, 0, 1]).
+    Do not add explanation.
+
+    Documents:
+    {[(i, d.page_content[:500]) for i, d in enumerate(documents)]}
+    """
+    response = chat.invoke(prompt)
+    try:
+        ranked_indices = eval(response.content)
+        ranked_indices = [
+            i for i in ranked_indices
+            if isinstance(i, int) and 0 <= i < len(documents)
+        ]
+    except Exception:
+        ranked_indices = list(range(len(documents)))  # fallback
+
+    top_documents = [documents[i] for i in ranked_indices[:5]]
+
+    document_pages = [doc.page_content for doc in top_documents]
 
     return {"documents": document_pages}
 
@@ -75,9 +110,11 @@ def build_graph():
 
     graph.add_node("retrieve_documents", retrieve_documents)
     graph.add_node("generate_answer", generate_answer)
+    graph.add_node("re_rank_documents", re_rank_documents)
 
     graph.add_edge(START, "retrieve_documents")
-    graph.add_edge("retrieve_documents", "generate_answer")
+    graph.add_edge("retrieve_documents", "re_rank_documents")
+    graph.add_edge("re_rank_documents", "generate_answer")
     graph.add_edge("generate_answer", END)
 
     memory = MemorySaver()
