59c091827c5b79d6c84835976686fb8f9166f5e4
jasman <jasman0603@gmail.com>
2026-02-08 22:52:37 +0700

    RAG Multi models

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ from dotenv import load_dotenv
 from graph_model import build_graph
 from uuid import uuid4
 from gtts import gTTS
+import logging
+from datetime import datetime
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+logging.basicConfig(
+    filename=os.path.join(LOG_DIR, "error_log.txt"),
+    level=logging.ERROR,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+
 
 load_dotenv()
 st.set_page_config(page_title="PDF Chatbot", page_icon="🤖", layout="centered")
@@ -27,33 +40,42 @@ for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
 
-user_input = st.chat_input("Ask something about the PDF...")
+error_message = {}
+try:
+    user_input = st.chat_input("Ask something about the PDF...")
 
-if user_input:
-    st.session_state.messages.append({"role": "user", "content": user_input})
-    with st.chat_message("user"):
-        st.markdown(user_input)
+    if user_input:
+        st.session_state.messages.append({"role": "user", "content": user_input})
+        with st.chat_message("user"):
+            st.markdown(user_input)
 
-    with st.spinner("Thinking..."):
-        result = graph.invoke(
-            {"messages": st.session_state.messages},
-            config={"configurable": {"thread_id": st.session_state.thread_id}}
-        )
+        with st.spinner("Thinking..."):
+            result = graph.invoke(
+                {"messages": st.session_state.messages},
+                config={"configurable": {"thread_id": st.session_state.thread_id}}
+            )
 
-        assistant_reply = result["messages"][-1].content
+            assistant_reply = result["messages"][-1].content
 
-    st.session_state.messages.append({"role": "assistant", "content": assistant_reply})
-    with st.chat_message("assistant"):
-        st.markdown(assistant_reply)
-    if st.session_state.language == "en":
-        assistant_reply = st.session_state.messages[-1]["content"]
-        tts = gTTS(text=assistant_reply, lang="en")
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
-            tts.save(tmp_file.name)
-            audio_path = tmp_file.name
-        with open(audio_path, "rb") as audio_file:
-            st.audio(audio_file.read(), format="audio/mp3")
-        os.remove(audio_path)
-
+        st.session_state.messages.append({"role": "assistant", "content": assistant_reply})
+        with st.chat_message("assistant"):
+            st.markdown(assistant_reply)
+        if st.session_state.language == "en":
+            assistant_reply = st.session_state.messages[-1]["content"]
+            tts = gTTS(text=assistant_reply, lang="en")
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp_file:
+                tts.save(tmp_file.name)
+                audio_path = tmp_file.name
+            with open(audio_path, "rb") as audio_file:
+                st.audio(audio_file.read(), format="audio/mp3")
+            os.remove(audio_path)
+    
+except Exception as e:
+    st.error(f"An error occurred: please try again later.")
+    error_text = str(e)
+    logging.error(
+        f"Thread ID: {st.session_state.get('thread_id')} | Error: {error_text}",
+        exc_info=True
+    )
 
         
